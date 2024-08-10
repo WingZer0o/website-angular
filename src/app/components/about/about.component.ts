efc5e0c9d3dc247e2d9bc53fc300cb3177a7b25e
Mike Mulchrone <mtmulch0191@outlook.com>
2024-08-09 23:46:51 -0400

    adding picture to avatar

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../modules/material/material.module';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
