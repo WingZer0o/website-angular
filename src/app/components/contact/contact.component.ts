2bc742a60a090263aad53e477130498fc8a3219a
Mike Mulchrone <mtmulch0191@outlook.com>
2024-08-09 22:58:02 -0400

    basic contact me form

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MaterialModule } from '../../modules/material/material.module';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
+  public contactForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      name: new FormControl<string | null>(null),
+      email: new FormControl<string | null>(null),
+      message: new FormControl<string | null>(null),
+    });
+  }
 }
--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import { MatToolbarModule } from "@angular/material/toolbar";
@@ -10,7 +13,10 @@ const allModules = [
   MatIconModule,
   MatSlideToggleModule,
   MatButtonModule,
-  MatMenuModule
+  MatMenuModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule
 ];
 
 @NgModule({
